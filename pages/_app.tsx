b95539328d6e14e07b61d285d2e32ad0c330e55e
Tyler Corley <tyler_corley@me.com>
2021-09-12 11:35:46 -0700

    Proper light/dark theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import theme from '../utils/theme';
 
 function RolanDev({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeProvider
         options={{
           useSystemColorMode: true,
